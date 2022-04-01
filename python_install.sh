def964efee25e1a59b947f8750ea6195ca022290
dzarpelon <zarpelon@outlook.com>
2022-04-01 17:27:49 +0200

    First commit

---
+++ python_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# this script assumes you have homebrew installed. For an automated way of installing homebrew check my repo https://github.com/dzarpelon/homebrew
+
+#test if python is installed and if it's not, install it.
